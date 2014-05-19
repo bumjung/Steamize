551dabc45aaff633e774444fd07f7fb61a13fb5d
Paul <bumj94@gmail.com>
2014-05-19 19:33:02 -0400

    redirect main page home to summary

--- steam_projects/urls.py
+++ steam_projects/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
 	url(r'^inventory/', include('inventory.urls')),
 	url(r'^summary/', include('summary.urls')),
+	url(r'^$', include('summary.urls'))
 )
