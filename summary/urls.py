e8092e4df86adcd5e75ec1cd9e632bebc64d1e70
Paul <bumj94@gmail.com>
2015-01-03 14:34:16 -0500

    fix variable name

--- summary/urls.py
+++ summary/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
           url(r'^$', views.index, name='index'),
           url(r'get_profile/$', views.get_profile, name='get_profile'),
           url(r'profile/(?P<steam_id>\d{17})/$', cache_page(60*30)(views.profile), name='profile'),
-          url(r'gameInfo/(?P<steam_id>\d{17})/(?P<app_id>\d+)/$', views.game, name='game'),
+          url(r'game/(?P<steam_id>\d{17})/(?P<app_id>\d+)/$', views.gameInfo, name='gameInfo'),
 		  url(r'privacy/', views.privacy, name='privacy'),
 		  url(r'about/', views.about, name='about'))
